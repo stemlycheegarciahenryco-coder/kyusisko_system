da21eafa38d0f896addde928736ad4567af575e8
stemlycheegarciahenryco-coder <stemlychee.garciahenryco@gmail.com>
2026-06-08 18:43:42 +0800

    adding a patch method in server.js

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
